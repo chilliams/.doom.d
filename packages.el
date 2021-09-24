1eff9bd9d5c2035828368c32a556e2d5ff88c67a
Chris Williams <cwilliams@yext.com>
2021-09-24 11:35:47 -0400

    Add protobuf-mode

--- packages.el
+++ packages.el
@@ -21,6 +21,8 @@
 (package! emacs-bazel-mode
   :recipe (:host github :repo "bazelbuild/emacs-bazel-mode"))
 
+(package! protobuf-mode)
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
