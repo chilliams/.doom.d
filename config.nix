27e1e80007c713e2788183f22a51f6e1668a640c
Christopher Williams <cwilliams@yext.com>
2025-06-12 11:08:24 -0400

    Add config.nix

---
+++ config.nix
@@ -0,0 +1,22 @@
+{
+  packageOverrides = pkgs: rec {
+    all = pkgs.buildEnv {
+      name = "all";
+      paths = [
+        pkgs.emacs
+        pkgs.gitAndTools.gitFull
+        pkgs.graphviz
+        pkgs.grpcurl
+        pkgs.nodejs_24
+        pkgs.pandoc
+        pkgs.pprof
+        pkgs.semgrep
+        pkgs.tree
+        pkgs.wget
+        (pkgs.yarn.override {
+          nodejs = null;
+        })
+      ];
+    };
+  };
+}
